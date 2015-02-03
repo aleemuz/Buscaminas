d5d3d3c9fd7c0093c0b09a7c37fbc8211586e151
aLeemuz <denuevo_samm@hotmail.com>
2015-02-03 09:59:47 -0700

    Nuevo Buscaminas, medio terminado

--- Buscaminas/src/buscaminas/Buscaminas.java
+++ Buscaminas/src/buscaminas/Buscaminas.java
@@ -16,7 +16,7 @@ public class Buscaminas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       new VentanaBuscaminas().setVisible(true);
     }
     
 }
---
+++ buscaminas1/src/buscaminas.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseListener;
+import javax.swing.*;
+import java.awt.event.*;
+
+
+public class buscaminas extends JFrame {
+    
+}
